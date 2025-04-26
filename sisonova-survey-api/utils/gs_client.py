590c947dc521054ca2ab194d603fdc1dd7ab39d1
Jason Joannou <jjoannou1099@gmail.com>
2025-04-26 15:47:26 +0200

    Google spreadsheet client

---
+++ sisonova-survey-api/utils/gs_client.py
@@ -0,0 +1,50 @@
+import os
+from typing import List, Optional
+
+import gspread
+from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
+
+load_dotenv()
+
+SCOPE: List[str] = os.getenv("GOOGLE_API_SCOPE")
+
+
+def load_gs_client(scope: Optional[List[str]] = None) -> gspread.Client:
+    """
+    Creates and returns an authenticated Google Sheets client using a service account.
+
+    Args:
+        scope (List[str]): A list of OAuth scopes required for accessing Google Sheets
+                           and Google Drive APIs. Common scopes include:
+                           - "https://spreadsheets.google.com/feeds"
+                           - "https://www.googleapis.com/auth/drive"
+
+    Returns:
+        gspread.Client: An authorized gspread client that can be used to access and modify Google Sheets.
+
+    Raises:
+        FileNotFoundError: If the service account credentials file is not found.
+        ValueError: If the credentials are invalid or improperly formatted.
+        Exception: For any other unexpected errors during client creation.
+    """
+    try:
+        if scope is None:
+            scope = SCOPE
+
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            filename="../sisonova-api-credentials.json", scopes=scope
+        )
+        client = gspread.authorize(credentials)
+        return client
+
+    except FileNotFoundError as f:
+        raise FileNotFoundError(f"Service account credentials file not found str{f}")
+
+    except ValueError as v:
+        raise ValueError(f"Invalid credentials file format {str(v)}")
+
+    except Exception as e:
+        raise Exception(
+            f"An unexpected error occurred while loading Google Sheets client: {str(e)}"
+        )
