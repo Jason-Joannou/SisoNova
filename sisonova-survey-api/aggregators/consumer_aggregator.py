bcf731f96194427d6b30f6396105700bd6d2d1d7
Jason Joannou <jjoannou1099@gmail.com>
2025-04-26 16:00:18 +0200

    Aggregator files to pull from spreadsheet

---
+++ sisonova-survey-api/aggregators/consumer_aggregator.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+from utils.gs_client import load_gs_client
+
+# The idea is to build a story line for each of the sections in the survey
+# So whats the storyline
+# Ie this is our average male user
+# on average he supports x
+# on average he struggles with y
+# Then we conclude our findings at the end
