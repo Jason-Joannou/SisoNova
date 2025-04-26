953ea21ebc56db0bedb0144b39ac4179f76eae2d
Jason Joannou <jjoannou1099@gmail.com>
2025-04-26 16:42:35 +0200

    config file

---
+++ sisonova-survey-api/utils/config.py
@@ -0,0 +1,8 @@
+DEMOGRAPHIC_COLUMNS = [
+    "What age group do you belong to?",
+    "What is your gender?",
+    "Which province do you currently live in?",
+    "How would you best describe the area you live?",
+    "What is your current employment status?",
+    "What is your highest level of education completed?",
+]
