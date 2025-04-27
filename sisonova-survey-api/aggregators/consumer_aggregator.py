00f1db5659057758c385ad17429dec350655e747
Jason Joannou <jjoannou1099@gmail.com>
2025-04-27 19:28:14 +0200

    Personal Income Calculations

--- sisonova-survey-api/aggregators/consumer_aggregator.py
+++ sisonova-survey-api/aggregators/consumer_aggregator.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from utils.gs_client import load_gs_client
@@ -32,3 +33,31 @@ def get_male_demographic_statistics(df: pd.DataFrame):
     most_frequent_education_level = df["EDUCATION_COMPLETED"].mode()[0]
 
     # Personal and household income
+
+    most_frequent_personal_income = df["PERSONAL_INCOME"].mode()[0]
+    most_frequent_household_income = df["HOUSEHOLD_INCOME"].mode()[0]
+    most_frequent_source_of_income = df["SOURCE_OF_INCOME"].mode()[0]
+
+    people_in_household_list = df["PEOPLE_IN_HOUSEHOLD"].to_list()
+    formatted_people_in_household_list = []
+
+    for number in people_in_household_list:
+        if len(number) > 1:
+            formatted_number = int(number[0])
+            formatted_people_in_household_list.append(formatted_number)
+        else:
+            formatted_people_in_household_list.append(int(number))
+
+    average_people_in_household = np.mean(formatted_people_in_household_list)
+
+    earn_income_in_household = df["EARN_INCOME_IN_HOUSEHOLD"].to_list()
+    formatted_earn_income_in_household = []
+
+    for number in earn_income_in_household:
+        if len(number) > 1:
+            formatted_number = int(number)
+            formatted_earn_income_in_household.append(formatted_number)
+        else:
+            formatted_earn_income_in_household.append(int(number))
+
+    average_income_in_household = np.mean(formatted_earn_income_in_household)
