7aa628226f498579d2f94298daead2be0cc4af48
Jason Joannou <jjoannou1099@gmail.com>
2025-04-27 19:43:22 +0200

    Mapping for income management

--- sisonova-survey-api/utils/consumer_survey_mappings.py
+++ sisonova-survey-api/utils/consumer_survey_mappings.py
@@ -1,6 +1,10 @@
 from typing import List
 
-from utils.config import DEMOGRAPHIC_COLUMNS, PERSONAL_AND_HOUSEHOLD_INCOME_COLUMNS
+from utils.config import (
+    DEMOGRAPHIC_COLUMNS,
+    INCOME_MANAGEMENT_COLUMNS,
+    PERSONAL_AND_HOUSEHOLD_INCOME_COLUMNS,
+)
 
 
 class SurveyMappingError(Exception):
@@ -62,11 +66,40 @@ def map_personal_and_household_income_to_standard(columns_to_map: List[str]):
         PERSONAL_AND_HOUSEHOLD_INCOME_COLUMNS
     ):  # Check to see if we have all the columns
         raise SurveyMappingError(
-            f"Personal and Household Income columns do not match in length. {len(columns_to_keep)} != {len(PERSONAL_AND_HOUSEHOLD_INCOME)} (truth)"
+            f"Personal and Household Income columns do not match in length. {len(columns_to_keep)} != {len(PERSONAL_AND_HOUSEHOLD_INCOME_COLUMNS)} (truth)"
         )
 
     new_columns = [
         PERSONAL_AND_HOUSEHOLD_INCOME_COLUMNS[value] for value in columns_to_keep
     ]
+
+    return new_columns
+
+
+def map_income_managment_to_standard(columns_to_map: List[str]):
+    """
+    This method maps the survey column to standardised pandas columns to make it easier for aggregating.
+
+    Args:
+        columns_to_map (List[str]): The columns coming from the google sheets.
+
+    Returns:
+        List[str]: The newly mapped column names as a list of strings
+    """
+
+    columns_to_keep = []
+
+    for column in columns_to_map:
+        if column in INCOME_MANAGEMENT_COLUMNS:
+            columns_to_keep.append(column)
+
+    if len(columns_to_keep) != len(
+        INCOME_MANAGEMENT_COLUMNS
+    ):  # Check to see if we have all the columns
+        raise SurveyMappingError(
+            f"Personal and Household Income columns do not match in length. {len(columns_to_keep)} != {len(INCOME_MANAGEMENT_COLUMNS)} (truth)"
+        )
+
+    new_columns = [INCOME_MANAGEMENT_COLUMNS[value] for value in columns_to_keep]
 
     return new_columns
