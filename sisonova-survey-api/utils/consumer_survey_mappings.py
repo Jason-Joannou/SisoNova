d5fcd46aeea55d4246f880b96a532968b9de81f1
Jason Joannou <jjoannou1099@gmail.com>
2025-04-27 20:07:50 +0200

    Added all column mappings
    Might need to ask chatGPT to reword the column names to be better

--- sisonova-survey-api/utils/consumer_survey_mappings.py
+++ sisonova-survey-api/utils/consumer_survey_mappings.py
@@ -2,8 +2,12 @@ from typing import List
 
 from utils.config import (
     DEMOGRAPHIC_COLUMNS,
+    FINANCIAL_ACCESS_COLUMNS,
+    FINANCIAL_BARRIERS_COLUMNS,
     INCOME_MANAGEMENT_COLUMNS,
     PERSONAL_AND_HOUSEHOLD_INCOME_COLUMNS,
+    PHSYCOLOGICAL_BARRIERS_COLUMNS,
+    TECHNOLOGY_UNDERSTANDING_COLUMNS,
 )
 
 
@@ -101,5 +105,121 @@ def map_income_managment_to_standard(columns_to_map: List[str]):
         )
 
     new_columns = [INCOME_MANAGEMENT_COLUMNS[value] for value in columns_to_keep]
+
+    return new_columns
+
+
+def map_financial_access_to_standard(columns_to_map: List[str]):
+    """
+    This method maps the survey column to standardised pandas columns to make it easier for aggregating.
+
+    Args:
+        columns_to_map (List[str]): The columns coming from the google sheets.
+
+    Returns:
+        List[str]: The newly mapped column names as a list of strings
+    """
+
+    columns_to_keep = []
+
+    for column in columns_to_map:
+        if column in FINANCIAL_ACCESS_COLUMNS:
+            columns_to_keep.append(column)
+
+    if len(columns_to_keep) != len(
+        FINANCIAL_ACCESS_COLUMNS
+    ):  # Check to see if we have all the columns
+        raise SurveyMappingError(
+            f"Personal and Household Income columns do not match in length. {len(columns_to_keep)} != {len(FINANCIAL_ACCESS_COLUMNS)} (truth)"
+        )
+
+    new_columns = [FINANCIAL_ACCESS_COLUMNS[value] for value in columns_to_keep]
+
+    return new_columns
+
+
+def map_financial_barriers_to_standard(columns_to_map: List[str]):
+    """
+    This method maps the survey column to standardised pandas columns to make it easier for aggregating.
+
+    Args:
+        columns_to_map (List[str]): The columns coming from the google sheets.
+
+    Returns:
+        List[str]: The newly mapped column names as a list of strings
+    """
+
+    columns_to_keep = []
+
+    for column in columns_to_map:
+        if column in FINANCIAL_BARRIERS_COLUMNS:
+            columns_to_keep.append(column)
+
+    if len(columns_to_keep) != len(
+        FINANCIAL_BARRIERS_COLUMNS
+    ):  # Check to see if we have all the columns
+        raise SurveyMappingError(
+            f"Personal and Household Income columns do not match in length. {len(columns_to_keep)} != {len(FINANCIAL_BARRIERS_COLUMNS)} (truth)"
+        )
+
+    new_columns = [FINANCIAL_BARRIERS_COLUMNS[value] for value in columns_to_keep]
+
+    return new_columns
+
+
+def map_technology_understanding_to_standard(columns_to_map: List[str]):
+    """
+    This method maps the survey column to standardised pandas columns to make it easier for aggregating.
+
+    Args:
+        columns_to_map (List[str]): The columns coming from the google sheets.
+
+    Returns:
+        List[str]: The newly mapped column names as a list of strings
+    """
+
+    columns_to_keep = []
+
+    for column in columns_to_map:
+        if column in TECHNOLOGY_UNDERSTANDING_COLUMNS:
+            columns_to_keep.append(column)
+
+    if len(columns_to_keep) != len(
+        TECHNOLOGY_UNDERSTANDING_COLUMNS
+    ):  # Check to see if we have all the columns
+        raise SurveyMappingError(
+            f"Personal and Household Income columns do not match in length. {len(columns_to_keep)} != {len(TECHNOLOGY_UNDERSTANDING_COLUMNS)} (truth)"
+        )
+
+    new_columns = [TECHNOLOGY_UNDERSTANDING_COLUMNS[value] for value in columns_to_keep]
+
+    return new_columns
+
+
+def map_phsycological_barriers_to_standard(columns_to_map: List[str]):
+    """
+    This method maps the survey column to standardised pandas columns to make it easier for aggregating.
+
+    Args:
+        columns_to_map (List[str]): The columns coming from the google sheets.
+
+    Returns:
+        List[str]: The newly mapped column names as a list of strings
+    """
+
+    columns_to_keep = []
+
+    for column in columns_to_map:
+        if column in PHSYCOLOGICAL_BARRIERS_COLUMNS:
+            columns_to_keep.append(column)
+
+    if len(columns_to_keep) != len(
+        PHSYCOLOGICAL_BARRIERS_COLUMNS
+    ):  # Check to see if we have all the columns
+        raise SurveyMappingError(
+            f"Personal and Household Income columns do not match in length. {len(columns_to_keep)} != {len(PHSYCOLOGICAL_BARRIERS_COLUMNS)} (truth)"
+        )
+
+    new_columns = [PHSYCOLOGICAL_BARRIERS_COLUMNS[value] for value in columns_to_keep]
 
     return new_columns
