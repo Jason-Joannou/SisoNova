1de159e030ebc950e72f6434127e7bfb9f1ea04e
Jason Joannou <jjoannou1099@gmail.com>
2025-04-27 20:02:25 +0200

    Added all questions from survey

--- sisonova-survey-api/utils/config.py
+++ sisonova-survey-api/utils/config.py
@@ -31,3 +31,54 @@ INCOME_MANAGEMENT_COLUMNS = {
     "What matters most to you when making a financial decision, like buying something big or lending money?": "FINANCIAL_DECISION",
     "How often do you feel guilty or regret buying something?": "PURCHASE_GUILT",
 }
+
+FINANCIAL_ACCESS_COLUMNS = {
+    "Do you have accounts with any of the following? (Select all that apply)": "BANK_ACCOUNTS",
+    "How many different bank accounts or money-related accounts do you use regularly?": "NUMBER_OF_ACCOUNTS",
+    "If you do have more than one account, why did you open them? (Select all that apply)": "REASON_FOR_MULTIPLE_ACCOUNTS",
+    "If you have more than one account, what do you use your different accounts for? (Select all that apply)": "MULTIPLE_ACCOUNT_USES",
+    "Which of the following do you trust most to keep your money safe? (Select all that apply)": "FINANCIAL_SERVICES_TRUST",
+    "Have you ever changed your bank or money service provider?": "CHANGED_ACCOUNTS",
+    "Which services do you use the most with your bank or money provider? (Select all that apply)": "FREQUENTLY_USED_FINANCIAL_SERVICES",
+    "Have you had any of these problems when using your bank or money provider? (Select all that apply)": "FINANCIAL_SERVICE_PROBLEMS",
+    "How often do you use mobile banking or financial apps (e.g., Capitec App, FNB App)?": "FINANCIAL_SERVICES_USAGE",
+    "Do you think banks and money services are made for people like you?": "FINANCIAL_SERVICES_MADE_FOR_YOU",
+}
+
+FINANCIAL_BARRIERS_COLUMNS = {
+    "Have you ever avoided getting a bank account, insurance, or loan because the paperwork was too confusing?": "AVOIDED_FINANCIAL_SERVICES",
+    "Is it easy for you to understand the papers and rules from banks and other money businesses?": "UNDERSTANDING_OF_FINANCIAL_SERVICES",
+    "Have you ever been asked to give documents (like your ID, proof of where you live, or payslip) and couldn’t?": "FINANCIAL_DOCUMENTATION_REQUIRED",
+    "When you think about using something new from the bank (like a loan or insurance), what worries you the most? (Select all that apply)": "WORRY_FOR_NEW_FINANCIAL_SERVICE",
+    "How sure are you that you can manage your money well?": "CAN_MANAGE_MONEY_WELL",
+    "If you don’t understand something about a bank or money service, what do you usually do?": "ACTION_FOR_NOT_UNDERSTANDING_FINANCIAL_SERVICE",
+    "Do you trust banks and other money services to treat you fairly and honestly?": "FAIRLY_TREATED",
+    "Have you ever had a bad experience with a bank or money service that made you not want to use them again?": "FINANCIAL_SERVICES_BAD_EXPERIENCE",
+    "Do you ever feel like banks or money services are not made for people in your situation or community?": "FINANCIAL_SERVICES_MADE_FOR_YOU",
+    "If banks and money services were explained more clearly and simply, would you be more likely to use them?": "FINANCIAL_SERVICES_ACCEPTANCE_IF_UNDERSTANABLE",
+}
+
+PHSYCOLOGICAL_BARRIERS_COLUMNS = {
+    "How comfortable are you with taking risks with your money (for example, investing in something that could make you more money, but also could lose money)?": "RISK_COMFORT",
+    "Do you think taking on debt (like a loan) can be a good idea if you can manage it well?": "TAKE_ON_DEBT_GOOD",
+    "What is your first thought when you hear the word “debt”?": "DEBT_EMOTION",
+    "If you needed money quickly, how would you feel about borrowing it (from a bank, family, or friends)?": "MONEY_BORROWING_FEELING",
+    "What does saving money mean to you? (Select all that apply)": "SAVING_MEANING",
+    "Do you think it’s possible to save money even if you don’t have a lot of money or your income changes often?": "POSSIBLE_TO_SAVE",
+    "When you get extra income (like a bonus, gift, or side job), what do you usually do first?": "EXTRA_INCOME_DECISION",
+    "Do you think saving money pays off in the long run?": "DOES_SAVING_PAYOFF",
+    "How do you decide if something is worth buying?": "WORTH_BUYING_RATIONAL",
+}
+
+TECHNOLOGY_UNDERSTANDING_COLUMNS = {
+    "How comfortable are you with using technology for money tasks (like sending money, checking your balance, or signing up for services)?": "COMFORTABLE_USING_FINANCIAL_APPS",
+    "Which of these devices do you use to manage your money? (Select all that apply)": "DEVICE_MANAGEMENT_FOR_MONEY",
+    "How often do you access the internet (on any device)?": "INTERNET_ACCESS_FREQUENCY",
+    "What’s the biggest problem you face when using digital tools for managing money (like apps, websites, or mobile money)?": "BIGGEST_DIGITAL_TOOL_PROBLEM",
+    "Have you ever stopped using a financial app or service because it was hard to understand or use?": "ACTION_FOR_NOT_UNDERSTANDING_FINANCIAL_SERVICE",
+    "If you needed to learn how to use a new financial app, how would you like to learn?": "PREFERRED_LEARNING_METHOD_FOR_NEW_FINANCIAL_SERVICE",
+    "What would encourage you to try a new financial app or tool? (Select all that apply)": "ENCOURAGEMENT_FOR_NEW_FINANCIAL_SERVICE",
+    "How much do you trust online platforms to keep your personal and financial information secure?": "ONLINE_FINANCIAL_SERVICES_TRUST_LEVELS",
+    "Do you ever avoid using digital tools for money because you're worried you might make a mistake (like sending money to the wrong person)?": "AVOID_DIGITAL_TOOLS",
+    "Which digital tools or apps do you use to manage your money? (Select all that apply)": "FINANCIAL_SERVICES_APPS",
+}
