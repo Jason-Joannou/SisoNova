590b11bae449bbde0fb03941cbca79154e83b835
Jason Joannou <jjoannou1099@gmail.com>
2025-04-28 10:59:47 +0200

    Added utils file

---
+++ sisonova-survey-api/utils/utils.py
@@ -0,0 +1,25 @@
+from collections import Counter
+from typing import List
+
+
+def format_checkbox_columns(checkbox_column: List[str]) -> str:
+    """
+    This formats and returns the most common string in a column
+
+    Args:
+        checkbox_column (List[str]): The column that contains the checkbox columns - strings are seperated by commas.
+
+    Returns:
+        str: The most common choice in the column
+    """
+
+    formatted_strings = []
+
+    for long_string in checkbox_column:
+        seperated_strings = long_string.split(",")
+        formatted_strings.extend(seperated_strings)
+
+    counter = Counter(formatted_strings)
+    most_common_string, _ = counter.most_common(1)[0]
+
+    return most_common_string
