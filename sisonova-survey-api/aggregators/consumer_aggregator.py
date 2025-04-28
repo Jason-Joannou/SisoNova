5f0dfb495c91e608eaf1df27d23a8d70148d3ba4
Jason Joannou <jjoannou1099@gmail.com>
2025-04-28 11:43:27 +0200

    Finished stats method

--- sisonova-survey-api/aggregators/consumer_aggregator.py
+++ sisonova-survey-api/aggregators/consumer_aggregator.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
+
 from utils.gs_client import load_gs_client
 from utils.utils import format_checkbox_columns
 
@@ -163,3 +164,190 @@ def get_gender_demographic_statistics(df: pd.DataFrame, gender: Optional[str] = 
         if not new_df["PURCHASE_REGRET_FREQUENCY"].empty
         else "No data"
     )
+
+    # Financial Access
+
+    stats["most_frequent_account_type"] = format_checkbox_columns(
+        new_df["ACCOUNT_TYPES"].to_list()
+    )
+    stats["most_frequent_active_accounts_count"] = (
+        new_df["ACTIVE_ACCOUNTS_COUNT"].mode()[0]
+        if not new_df["ACTIVE_ACCOUNTS_COUNT"].empty
+        else "No data"
+    )
+    stats["most_frequent_multiple_accounts_reason"] = format_checkbox_columns(
+        new_df["MULTIPLE_ACCOUNTS_REASONS"].to_list()
+    )
+    stats["most_frequent_account_usage_purpose"] = format_checkbox_columns(
+        new_df["ACCOUNT_USAGE_PURPOSES"].to_list()
+    )
+    stats["most_frequent_trusted_financial_service"] = format_checkbox_columns(
+        new_df["TRUSTED_FINANCIAL_SERVICES"].to_list()
+    )
+    stats["most_frequent_provider_switching"] = (
+        new_df["PROVIDER_SWITCHING_HISTORY"].mode()[0]
+        if not new_df["PROVIDER_SWITCHING_HISTORY"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_service"] = format_checkbox_columns(
+        new_df["MOST_USED_FINANCIAL_SERVICES"].to_list()
+    )
+    stats["most_frequent_financial_service_issue"] = format_checkbox_columns(
+        new_df["FINANCIAL_SERVICE_ISSUES"].to_list()
+    )
+    stats["most_frequent_digital_banking_usage"] = (
+        new_df["DIGITAL_BANKING_FREQUENCY"].mode()[0]
+        if not new_df["DIGITAL_BANKING_FREQUENCY"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_inclusion_perception"] = (
+        new_df["FINANCIAL_INCLUSION_PERCEPTION"].mode()[0]
+        if not new_df["FINANCIAL_INCLUSION_PERCEPTION"].empty
+        else "No data"
+    )
+
+    # Financial Barriers
+
+    stats["most_frequent_paperwork_avoidance"] = (
+        new_df["PAPERWORK_AVOIDANCE"].mode()[0]
+        if not new_df["PAPERWORK_AVOIDANCE"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_literacy"] = (
+        new_df["FINANCIAL_LITERACY_LEVEL"].mode()[0]
+        if not new_df["FINANCIAL_LITERACY_LEVEL"].empty
+        else "No data"
+    )
+    stats["most_frequent_documentation_barrier"] = (
+        new_df["DOCUMENTATION_BARRIERS"].mode()[0]
+        if not new_df["DOCUMENTATION_BARRIERS"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_service_concern"] = format_checkbox_columns(
+        new_df["FINANCIAL_SERVICE_CONCERNS"].to_list()
+    )
+    stats["most_frequent_financial_confidence"] = (
+        new_df["FINANCIAL_CONFIDENCE"].mode()[0]
+        if not new_df["FINANCIAL_CONFIDENCE"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_confusion_response"] = (
+        new_df["FINANCIAL_CONFUSION_RESPONSE"].mode()[0]
+        if not new_df["FINANCIAL_CONFUSION_RESPONSE"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_institution_trust"] = (
+        new_df["FINANCIAL_INSTITUTION_TRUST"].mode()[0]
+        if not new_df["FINANCIAL_INSTITUTION_TRUST"].empty
+        else "No data"
+    )
+    stats["most_frequent_negative_financial_experience"] = (
+        new_df["NEGATIVE_FINANCIAL_EXPERIENCES"].mode()[0]
+        if not new_df["NEGATIVE_FINANCIAL_EXPERIENCES"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_exclusion_feeling"] = (
+        new_df["FINANCIAL_EXCLUSION_FEELING"].mode()[0]
+        if not new_df["FINANCIAL_EXCLUSION_FEELING"].empty
+        else "No data"
+    )
+    stats["most_frequent_clarity_impact"] = (
+        new_df["CLARITY_IMPACT_ON_USAGE"].mode()[0]
+        if not new_df["CLARITY_IMPACT_ON_USAGE"].empty
+        else "No data"
+    )
+
+    # Psychological Barriers
+
+    stats["most_frequent_financial_risk_tolerance"] = (
+        new_df["FINANCIAL_RISK_TOLERANCE"].mode()[0]
+        if not new_df["FINANCIAL_RISK_TOLERANCE"].empty
+        else "No data"
+    )
+    stats["most_frequent_debt_attitude"] = (
+        new_df["DEBT_ATTITUDE"].mode()[0]
+        if not new_df["DEBT_ATTITUDE"].empty
+        else "No data"
+    )
+    stats["most_frequent_debt_perception"] = (
+        new_df["DEBT_PERCEPTION"].mode()[0]
+        if not new_df["DEBT_PERCEPTION"].empty
+        else "No data"
+    )
+    stats["most_frequent_borrowing_comfort"] = (
+        new_df["BORROWING_COMFORT"].mode()[0]
+        if not new_df["BORROWING_COMFORT"].empty
+        else "No data"
+    )
+    stats["most_frequent_saving_perception"] = format_checkbox_columns(
+        new_df["SAVING_PERCEPTION"].to_list()
+    )
+    stats["most_frequent_saving_possibility_belief"] = (
+        new_df["SAVING_POSSIBILITY_BELIEF"].mode()[0]
+        if not new_df["SAVING_POSSIBILITY_BELIEF"].empty
+        else "No data"
+    )
+    stats["most_frequent_windfall_usage"] = (
+        new_df["WINDFALL_USAGE"].mode()[0]
+        if not new_df["WINDFALL_USAGE"].empty
+        else "No data"
+    )
+    stats["most_frequent_saving_benefit_belief"] = (
+        new_df["SAVING_BENEFIT_BELIEF"].mode()[0]
+        if not new_df["SAVING_BENEFIT_BELIEF"].empty
+        else "No data"
+    )
+    stats["most_frequent_purchase_decision_criteria"] = (
+        new_df["PURCHASE_DECISION_CRITERIA"].mode()[0]
+        if not new_df["PURCHASE_DECISION_CRITERIA"].empty
+        else "No data"
+    )
+
+    # Technology Understanding
+
+    stats["most_frequent_fintech_comfort_level"] = (
+        new_df["FINTECH_COMFORT_LEVEL"].mode()[0]
+        if not new_df["FINTECH_COMFORT_LEVEL"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_management_device"] = format_checkbox_columns(
+        new_df["FINANCIAL_MANAGEMENT_DEVICES"].to_list()
+    )
+    stats["most_frequent_internet_usage"] = (
+        new_df["INTERNET_USAGE_FREQUENCY"].mode()[0]
+        if not new_df["INTERNET_USAGE_FREQUENCY"].empty
+        else "No data"
+    )
+    stats["most_frequent_digital_finance_challenge"] = (
+        new_df["DIGITAL_FINANCE_CHALLENGES"].mode()[0]
+        if not new_df["DIGITAL_FINANCE_CHALLENGES"].empty
+        else "No data"
+    )
+    stats["most_frequent_app_abandonment"] = (
+        new_df["APP_ABANDONMENT_EXPERIENCE"].mode()[0]
+        if not new_df["APP_ABANDONMENT_EXPERIENCE"].empty
+        else "No data"
+    )
+    stats["most_frequent_fintech_learning_preference"] = (
+        new_df["FINTECH_LEARNING_PREFERENCE"].mode()[0]
+        if not new_df["FINTECH_LEARNING_PREFERENCE"].empty
+        else "No data"
+    )
+    stats["most_frequent_fintech_adoption_motivator"] = format_checkbox_columns(
+        new_df["FINTECH_ADOPTION_MOTIVATORS"].to_list()
+    )
+    stats["most_frequent_digital_security_trust"] = (
+        new_df["DIGITAL_SECURITY_TRUST"].mode()[0]
+        if not new_df["DIGITAL_SECURITY_TRUST"].empty
+        else "No data"
+    )
+    stats["most_frequent_digital_error_anxiety"] = (
+        new_df["DIGITAL_ERROR_ANXIETY"].mode()[0]
+        if not new_df["DIGITAL_ERROR_ANXIETY"].empty
+        else "No data"
+    )
+    stats["most_frequent_financial_app_used"] = format_checkbox_columns(
+        new_df["FINANCIAL_APPS_USED"].to_list()
+    )
+
+    return stats
