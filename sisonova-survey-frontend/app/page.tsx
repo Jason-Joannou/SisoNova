3841f65c91bd0529c543a1f86a587fecd57e0f8d
Jason Joannou <jjoannou@bscglobal.com>
2025-05-01 12:01:49 +0200

    new styling

--- sisonova-survey-frontend/app/page.tsx
+++ sisonova-survey-frontend/app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
   return (
 
     <div className="min-h-screen py-12">
-      <div className="container mx-auto px-4 max-w-4xl bg-white/80 rounded-xl shadow-lg py-8">
+      <div className="container mx-auto px-4 max-w-4xl bg-[#291009]/80 rounded-xl shadow-lg py-8">
       <motion.h1 
-        className="text-5xl md:text-7xl font-so text-center mb-6"
+        className="text-5xl md:text-7xl font-so text-center mb-2 text-[#24713F]"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
@@ -22,7 +22,8 @@ export default function Home() {
       </motion.h1>
       
       <motion.p 
-        className="text-xl md:text-2xl text-center text-muted-foreground mb-16 font-grape-nuts"
+        className="text-center mb-4 text-[#4A9051] font-oswald font-bold"
+        style={{ fontSize: '2rem' }}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.2 }}
@@ -36,10 +37,10 @@ export default function Home() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.4 }}
       >
-        <p className="text-2xl mb-6 font-grape-nuts">
+        <p className="text-3xl mb-6 font-oswald text-[#4A9051]">
           Let's go through the average 
-          <span className="inline-block mx-2 min-w-[150px]">
-            <Select onValueChange={(value) => setSelectedGender(value === 'all' ? undefined : value)}>
+          <span className="inline-block mx-2 min-w-[150px] text-[#24713F]" >
+            <Select onValueChange={(value) => setSelectedGender(value === 'all'? undefined : value)}>
               <SelectTrigger className="w-full">
                 <SelectValue placeholder="South African" />
               </SelectTrigger>
