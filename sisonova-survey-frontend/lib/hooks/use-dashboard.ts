38f3a7c1fb0b3e71113fbce3b6907eca756c123e
Jason Joannou <jjoannou1099@gmail.com>
2025-05-04 14:43:11 +0200

    Use dashboard hook

---
+++ sisonova-survey-frontend/lib/hooks/use-dashboard.ts
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { fetchDashboardData } from "@/services/dashboard-services";
+import { DashboardFilters } from "../types/api";
+import { DashboardResponse } from "../types/responses";
+
+export function useDashboard(dashboardFilters: DashboardFilters) {
+  const [data, setData] = useState<DashboardResponse>([{ name: "", count: 0 }]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetchDashboardData(dashboardFilters);
+        setData(response);
+      } catch (error) {
+        setError("Error fetching storyline");
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadData();
+  }, [
+    dashboardFilters.gender,
+    dashboardFilters.age_group,
+    dashboardFilters.province,
+  ]);
+
+  return { data, loading, error };
+}
