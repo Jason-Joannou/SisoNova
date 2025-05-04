d74bb2e50a571ccc9116da6cd0a3c2834268ecc6
Jason Joannou <jjoannou1099@gmail.com>
2025-05-04 16:01:04 +0200

    Updated demographics aggregator

--- sisonova-survey-api/aggregators/dashboard/demographics.py
+++ sisonova-survey-api/aggregators/dashboard/demographics.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from utils.config import DEMOGRAPHIC_COLUMNS
-from utils.utils import get_column_value_counts
+from utils.config import DEMOGRAPHIC_COLUMNS, PERSONAL_AND_HOUSEHOLD_INCOME_COLUMNS
+from utils.utils import format_number_columns, get_column_value_counts
 
 
 def process_demographic_columns(df: pd.DataFrame):
@@ -14,6 +14,29 @@ def process_demographic_columns(df: pd.DataFrame):
 
     results = {
         value: get_column_value_counts(column=value, df=df)
+        for _, value in DEMOGRAPHIC_COLUMNS.items()
+    }
+
+    return results
+
+
+def process_income_columns(df: pd.DataFrame):
+    """
+    This method creates counts for each column in the
+    income question in the survey and returns a dictionary as the result.
+
+    Args:
+        df (pd.DataFrame): DataFrame holding the survey data
+    """
+
+    avg_columns = ["HOUSEHOLD_SIZE", "INCOME_EARNERS_COUNT"]
+
+    results = {
+        value: (
+            format_number_columns(df[value].to_list())
+            if value in avg_columns
+            else get_column_value_counts(column=value, df=df)
+        )
         for _, value in DEMOGRAPHIC_COLUMNS.items()
     }
 
