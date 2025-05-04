473f45ebf97eb37b16c92160a45a90cce9656672
Jason Joannou <jjoannou1099@gmail.com>
2025-05-04 21:47:22 +0200

    Added dashboard router

--- sisonova-survey-api/app/routers/dashboard.py
+++ sisonova-survey-api/app/routers/dashboard.py
@@ -2,6 +2,7 @@ from typing import Dict, Optional, Union
 
 from aggregators.dashboard.demographics import (
     process_demographic_columns,
+    process_financial_access_columns,
     process_income_columns,
     process_income_management_columns,
 )
@@ -85,6 +86,35 @@ async def get_user_income_management(
         )
 
         results = process_income_management_columns(df=df)
+
+        response_data = {
+            "dashboard_response": results,
+            "message": f"Successfully retreieved demographic distributions for filters: gender: {gender}, age_group: {age_group}, province: {province}",
+        }
+
+        return JSONResponse(status_code=200, content=response_data)
+
+    except Exception as e:
+        return HTTPException(
+            status_code=500, detail=f"Error getting demographic information: {str(e)}"
+        )
+
+
+@router.get(
+    "/financial_access", response_model=None
+)  # Will define response model later
+async def get_user_financial_access(
+    gender: Optional[str] = None,
+    age_group: Optional[str] = None,
+    province: Optional[str] = None,
+):
+    try:
+        df = get_survey_results_into_df()
+        df = filter_survey_dataframe(
+            df=df, gender=gender, age_group=age_group, province=province
+        )
+
+        results = process_financial_access_columns(df=df)
 
         response_data = {
             "dashboard_response": results,
