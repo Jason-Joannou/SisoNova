942a6c45ac03ec990638f1d7a7e36d5d2fc3cd95
Jason Joannou <jjoannou1099@gmail.com>
2025-05-04 16:01:11 +0200

    Updated router

--- sisonova-survey-api/app/routers/dashboard.py
+++ sisonova-survey-api/app/routers/dashboard.py
@@ -13,7 +13,34 @@ router = APIRouter(
 
 
 @router.get("/demographics", response_model=None)  # Will define response model later
-async def get_storyline(
+async def get_user_demographics(
+    gender: Optional[str] = None,
+    age_group: Optional[str] = None,
+    province: Optional[str] = None,
+):
+    try:
+        df = get_survey_results_into_df()
+        df = filter_survey_dataframe(
+            df=df, gender=gender, age_group=age_group, province=province
+        )
+
+        results = process_demographic_columns(df=df)
+
+        response_data = {
+            "dashboard_response": results,
+            "message": f"Successfully retreieved demographic distributions for filters: gender: {gender}, age_group: {age_group}, province: {province}",
+        }
+
+        return JSONResponse(status_code=200, content=response_data)
+
+    except Exception as e:
+        return HTTPException(
+            status_code=500, detail=f"Error getting demographic information: {str(e)}"
+        )
+
+
+@router.get("/income", response_model=None)  # Will define response model later
+async def get_user_income(
     gender: Optional[str] = None,
     age_group: Optional[str] = None,
     province: Optional[str] = None,
