f790c85c8917df308210ef9f536e9c5a303aa740
Jason Joannou <jjoannou1099@gmail.com>
2025-05-04 12:17:06 +0200

    New method to calculate demographic distributions

--- sisonova-survey-api/aggregators/dashboard/demographics.py
+++ sisonova-survey-api/aggregators/dashboard/demographics.py
@@ -1,5 +1,23 @@
 import pandas as pd
+from utils.config import DEMOGRAPHIC_COLUMNS
 from utils.utils import get_column_value_counts, get_survey_results_into_df
+
+
+def process_demographic_columns(df: pd.DataFrame):
+    """
+    This method creates counts for each column in the
+    demographic question in the survey and returns a dictionary as the result.
+
+    Args:
+        df (pd.DataFrame): DataFrame holding the survey data
+    """
+
+    results = {
+        value: get_column_value_counts(column=value, df=df)
+        for _, value in DEMOGRAPHIC_COLUMNS.items()
+    }
+
+    return results
 
 
 def process_gender_distribution(df: pd.DataFrame):
