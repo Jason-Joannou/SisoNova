c17f09a032752d84582001036e8b5b36393f47a6
Jason Joannou <jjoannou@bscglobal.com>
2025-05-14 16:02:23 +0200

    base api routes

--- poc/api/app.py
+++ poc/api/app.py
@@ -1,11 +1,37 @@
+from utils import logger_config
 import logging
-from fastapi import FastAPI, JSONResponse
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from middleware.middleware import APIMiddleware
+from contextlib import asynccontextmanager
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup code
+    logger.info("API starting up")
+    yield
+    # Shutdown code
+    logger.info("API shutting down")
+
 
 app = FastAPI(
     title="SisoNova Service",
     description="API for SisoNova core services",
     version="1.0.0",
+    lifespan=lifespan
+    
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Add middleware
@@ -33,3 +59,9 @@ async def root():
 @app.get("/health")
 async def health():
     return JSONResponse(status_code=200, content={"message": "Healthy"})
+
+
+if __name__ == "__main__":
+    import uvicorn
+    logger.info("Starting API server")
+    uvicorn.run("app:app", host="127.0.0.1", port=3000, reload=True)
