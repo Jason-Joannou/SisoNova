a2cb82606006bd4ec2cd06e81b37fa101fd4236d
Jason Joannou <jjoannou@bscglobal.com>
2025-05-21 23:27:09 +0200

    Twilio template manager

--- poc/api/utils/twilio_templates.py
+++ poc/api/utils/twilio_templates.py
@@ -1,18 +1,52 @@
 from typing import Optional
+from utils.language_config import load_language_config, get_template_validation
 
 
 class TwilioTemplateManager:
 
-    def __init__(self, language: Optional[str] = None) -> None:
-        self.preferred_language, self.current_template, self.previous_template = (
-            self.init_template_manager()
-        )
-
-    def init_template_manager(self):
-        pass
+    def __init__(self, user_exists: bool, user_response: str, current_template: Optional[str], language: Optional[str] = None) -> None:
+        self.user_exists = user_exists
+        self.preferred_language = self._language_selection_mapping(language=language)
+        self.user_response = user_response
+        self.templates = {}
+        self.current_template_name = self.set_template_name(template_name=current_template)
 
     def _language_selection_mapping(self, language: str):
 
         language_codes = {"English": "en", "Afrikaans": "af", "Zulu": "zl"}
 
+        if language not in language_codes:
+            language = "English"
+
         return language_codes[language]
+    
+    def _set_message_template(self, template_name: str) -> None:
+        self.templates = load_language_config()[self.preferred_language][template_name]
+
+    def _validate_user_response(self) -> bool:
+        validation_obj = get_template_validation(template_name=self.current_template_name)
+
+        try:
+            validation_obj.inbound_validator(self.user_response)
+            return True
+        except ValueError:
+            return False
+
+
+    def set_template_name(self, template_name: str) -> None:
+        if template_name is None:
+            template_name = "unregistered_number_language_selector_template"
+        self.current_template_name = template_name
+        self._set_message_template(template_name=template_name)
+
+    def get_template_message(self):
+        
+        if self._validate_user_response():
+            return self.templates["template_message"]
+        
+        validation_obj = get_template_validation(template_name=self.current_template_name)
+        error_message = self.templates["error_message"].format(validation_obj.validation_message)
+        return error_message
+
+
+        
