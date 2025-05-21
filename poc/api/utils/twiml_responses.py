ab20e5a842ca511fd0d29d44eee72d048de06b19
Jason Joannou <jjoannou@bscglobal.com>
2025-05-22 00:58:59 +0200

    update to responses

--- poc/api/utils/twiml_responses.py
+++ poc/api/utils/twiml_responses.py
@@ -1,7 +1,6 @@
 # This might be deleted later but will be used for
 # testing while we wait for whatsapp business approval
-
-from models.inbound_responses import LanguageSelector
+from typing import List, Dict
 from twilio.twiml.messaging_response import (
     Body,
     Media,
@@ -10,43 +9,20 @@ from twilio.twiml.messaging_response import (
     Redirect,
 )
 
-TEMPLATES = {
-    "unregistered_number_language_selector_template": {
-        "inbound_response_model": LanguageSelector,
-        "template_error_message": "error",
-    }
-}
+def generate_twiml_message(msgs: List[Dict[str, str]]):
+    response = MessagingResponse()
+    message = Message()
 
+    for msg in msgs:
+        for msg_type, msg_content in msg.items():
+            if msg_type == "body":
+                message.body(msg_content)
+            elif msg_type == "media_url":
+                message.media(msg_content)
+            elif msg_type == "redirect":
+                message.redirect(msg_content)
 
-def twiml_image_response() -> str:
-    twiml_response = """
-    <?xml version="1.0" encoding="UTF-8"?>
-        <Response>
-            <Message>
-                <Body>NASA Earth image</Body>
-                <Media>https://images-assets.nasa.gov/image/PIA00342/PIA00342~thumb.jpg</Media>
-            </Message>
-        </Response>
-    """
-    return twiml_response
+    response.message(message)
 
+    return str(response)
 
-def twiml_unregistered_response() -> str:
-    twiml_response = """
-    <?xml version ="1.0" encoding="UTF-8"?>
-        <Response>
-            <Message>
-                <Body>
-                    Hi there 👋 , and welcome to SisoNova’s financial assistance service.
-
-We believe that financial conversations are most effective when they happen in a language you’re comfortable with. 
-
-To help you feel confident and understood every step of the way, please select your preferred language to continue. 
-
-(You can change this anytime later.)
-                </Body>
-            </Message>
-        </Response>
-    """
-
-    return twiml_response
