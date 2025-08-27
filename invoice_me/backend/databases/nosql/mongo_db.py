c1922ca32072661548bb629626f27d95b0c7c56b
Jason Joannou <jjoannou@bscglobal.com>
2025-08-27 17:58:38 +0200

    Updated client

--- invoice_me/backend/databases/nosql/mongo_db.py
+++ invoice_me/backend/databases/nosql/mongo_db.py
@@ -7,9 +7,11 @@ import os
 load_dotenv()
 
 class MongoDB:
-    def __init__(self):
-        self.db = self._initialise_client()
-    
+    def __init__(self, database_name: str = "test_db"):
+        self.client = self._initialise_client()
+        self.database_name = database_name
+        self.db = self.client[self.database_name]
+
     def _initialise_client(self):
         try:
             uri = os.getenv("MONGO_CLIENT")
@@ -19,17 +21,23 @@ class MongoDB:
             return client
         except Exception as e:
             raise MongoDBConnectionError(e)
-        
+
+    def create_database(self):
+        """Return the database handle"""
+        return self.db
+
+    def create_collection(self, collection_name: str):
+        """Return the collection handle"""
+        if collection_name in self.db.list_collection_names():
+            return self.db[collection_name]
+        return self.db.create_collection(collection_name)
+
     def start_session(self):
         """Start a session for transactions or grouped operations"""
         return self.client.start_session()
-        
-    
-        
+
 
 if __name__ == "__main__":
-    mongo_db = MongoDB().db
-    print(mongo_db.list_database_names())
-
-
-
+    mongo_db = MongoDB()
+    db = mongo_db.create_database()
+    print("Existing databases:", mongo_db.client.list_database_names())
