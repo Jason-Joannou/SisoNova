2b4d67667873dea0e0391dc7cc88050b14ca5b5c
Jason Joannou <jjoannou@bscglobal.com>
2025-10-23 13:44:01 +0200

    moved models

--- reguMatch/api/tools/database/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field, field_validator
-from datetime import datetime
-from typing import List, Optional, Dict
-
-class AddEntryResponse(BaseModel):
-    success: bool
-    message: str
-    response_document: Dict
-    error: Optional[str] = None
