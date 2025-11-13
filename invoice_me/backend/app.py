62e82916d9c127ecf822b311bb35462fc2684bb4
Jason Joannou <jjoannou@bscglobal.com>
2025-11-13 12:13:03 +0200

    Added config and settings

--- invoice_me/backend/app.py
+++ invoice_me/backend/app.py
@@ -1,13 +1,32 @@
 from fastapi import FastAPI
 import os
 import uvicorn
+from config import AppSettings
+import logging
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+app_settings = AppSettings()
+app = FastAPI(
+    title=app_settings.app_name,
+    description=app_settings.app_description,
+    version=app_settings.app_version
+)
+
+logger.info(f"Application name: {app_settings.app_name}")
+logger.info(f"Application description: {app_settings.app_description}")
+logger.info(f"Application version: {app_settings.app_version}")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
+        host="localhost",
+        port=8000,
         reload=True
     )
---
+++ invoice_me/backend/config.py
@@ -0,0 +1,36 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict, PydanticBaseSettingsSource, JsonConfigSettingsSource
+from pydantic import Field
+from typing import Tuple, Type
+
+class Secrets(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", case_sensitive=False)
+    mongo_db_connection_string: str
+
+
+class AppSettings(BaseSettings):
+    model_config = SettingsConfigDict(json_file="./app_settings.json", json_file_encoding="utf-8", case_sensitive=False)
+    app_name: str = Field(..., description="The name of the application")
+    app_description: str = Field(..., description="A brief description of the application")
+    app_version: str = Field(..., description="The version of the application")
+
+    # Needed to read from JSON file
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+        # This is basically telling pydantic to read from a list of places
+        # By default it reads from init, env, dotenv, and file secrets
+        # We are adding JsonConfigSettingsSource to read from a JSON file
+        # We dont have to return all of them, we can just return what we want but for the sake of completeness we return them all
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            JsonConfigSettingsSource(settings_cls),  # This line enables JSON reading
+            file_secret_settings,
+        )
