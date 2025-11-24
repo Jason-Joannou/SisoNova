db832d41bc91364e9922baa0391f219f4267bf31
Jason Joannou <jjoannou@bscglobal.com>
2025-11-24 18:11:37 +0200

    middleware plus add route

--- invoice_me/backend/app.py
+++ invoice_me/backend/app.py
@@ -3,6 +3,8 @@ import os
 import uvicorn
 from config import AppSettings
 import logging
+from fastapi.middleware.cors import CORSMiddleware
+from routes.auth import router as auth_router
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
@@ -14,11 +16,20 @@ app = FastAPI(
     version=app_settings.app_version
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Or list your frontend origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 logger.info(f"Application name: {app_settings.app_name}")
 logger.info(f"Application description: {app_settings.app_description}")
 logger.info(f"Application version: {app_settings.app_version}")
 
 
+app.include_router(auth_router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
