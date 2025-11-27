b651fab602de4003d778765e7780d572cef1ce3f
Jason Joannou <jjoannou@bscglobal.com>
2025-11-27 10:33:07 +0200

    New operations

--- invoice_me/backend/database/mongo_operations.py
+++ invoice_me/backend/database/mongo_operations.py
@@ -1,6 +1,6 @@
 from database.mongo_client import MongoDBClient
-from models.users import CreateNewUser, UserProfile, VerifyUser
-from models.business import BusinessProfile
+from models.users import User, UserProfile, VerifyUser, UserUpdate
+from models.business import BusinessProfile, UpdateBusinessProfile
 from config import Secrets
 from datetime import datetime
 
@@ -16,7 +16,7 @@ async def does_user_exist(mongo_client: MongoDBClient, email: str) -> bool:
         print(f"Error checking user existence: {e}")
         return False
     
-async def create_user(mongo_client: MongoDBClient, new_user_data: CreateNewUser) -> str:
+async def create_user(mongo_client: MongoDBClient, new_user_data: User) -> str:
     """Create a new user in the database"""
     try:
         async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
@@ -61,3 +61,47 @@ async def get_user_profile(mongo_client: MongoDBClient, email: str) -> UserProfi
     except Exception as e:
         print(f"Error retrieving user profile: {e}")
         return None
+    
+
+async def update_user_information(mongo_client: MongoDBClient, email: str, user_update: UserUpdate) -> bool:
+    """Update base user information in the database"""
+
+    try:
+        async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
+            result = await db["users"].update_one(
+                {"email": email},
+                {"$set": user_update.model_dump(exclude_unset=True)}
+            )
+            return result.modified_count > 0
+    except Exception as e:
+        print(f"Error updating user information: {e}")
+        return False
+    
+
+async def add_business_profile_operation(mongo_client: MongoDBClient, email: str, business_profile: BusinessProfile):
+    try:
+        async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
+            result = await db["users"].update_one(
+                {"email": email},
+                {"$push" : {"business_profile": business_profile.model_dump()}},
+                {"$set": {"updated_at": datetime.utcnow().isoformat()}}
+            )
+            return result.modified_count > 0
+    except Exception as e:
+        print(f"Error updating business profile: {e}")
+        return False
+    
+
+async def update_business_profile_operation(mongo_client: MongoDBClient, email: str, company_name: str, business_profile: UpdateBusinessProfile):
+    try:
+        async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
+            result = await db["users"].update_one(
+                {"email": email, "business_profile.company_name": company_name},
+                {"$set": {"business_profile.$": business_profile.model_dump(exclude_unset=True)}},
+                {"$set": {"updated_at": datetime.utcnow().isoformat()}}
+            )
+            return result.modified_count > 0
+    except Exception as e:
+        print(f"Error updating business profile: {e}")
+        return False
+   
