d2bab0da94487a4d7bd978ebff905cd988ef2147
Jason Joannou <jjoannou1099@gmail.com>
2026-02-08 10:20:48 +0200

    Callback

---
+++ invoice_me/frontend/app/auth/callback/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import { createClient } from '@supabase/supabase-js'
+
+export async function GET(request: Request) {
+    const { searchParams, origin } = new URL(request.url)
+    const code = searchParams.get('code')
+
+    if (code) {
+        const options = {
+            auth: {
+                autoRefreshToken: true,
+                persistSession: true,
+                detectSessionInUrl: true
+            }
+        }
+        const supabase = createClient(
+            process.env.NEXT_PUBLIC_SUPABASE_URL!,
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+            options
+        )
+
+        await supabase.auth.exchangeCodeForSession(code)
+    }
+
+    return NextResponse.redirect(`${origin}/`)
+}
