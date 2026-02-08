f8e99851d589fc2dfdcec89344af85b8ad06ff52
Jason Joannou <jjoannou1099@gmail.com>
2026-02-09 00:24:00 +0200

    frontend changes

--- invoice_me/frontend/lib/utility/api/routes.ts
+++ invoice_me/frontend/lib/utility/api/routes.ts
@@ -1,9 +1,6 @@
 import { verify } from "crypto";
 
-
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
-
 export const API_ROUTES = {
-    me: `${API_BASE_URL}/user`,
-    addBusinessProfile: `${API_BASE_URL}/user/business-profile`,
+    me: `/user`,
+    addBusinessProfile: `/user/business-profile`,
 };
