d35988909554ee199657bb646eb9d0d273bfde2a
Jason Joannou <jjoannou1099@gmail.com>
2026-02-09 00:23:31 +0200

    updated operations

--- invoice_me/backend/database/mongo_operations.py
+++ invoice_me/backend/database/mongo_operations.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from database.mongo_client import MongoDBClient
 from models.users import User, UserProfile, VerifyUser, UserUpdate
 from models.business import (
@@ -8,30 +9,37 @@ from models.business import (
 from config import Secrets
 from datetime import datetime
 from typing import List
+import traceback
+import logging
+
+logger = logging.getLogger(__name__)
 
 secrets = Secrets()
 
 
-async def does_user_exist(mongo_client: MongoDBClient, email: str) -> bool:
-    """Check if a user with the given email exists in the database"""
-    try:
-        async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
-            user = await db["users"].find_one({"email": email})
-            return user is not None
-    except Exception as e:
-        print(f"Error checking user existence: {e}")
-        return False
-
-
-async def create_user(mongo_client: MongoDBClient, new_user_data: User) -> str:
+async def create_user(mongo_client: MongoDBClient, new_user_data: User) -> User:
     """Create a new user in the database"""
     try:
+        print(new_user_data.model_dump())
         async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
             result = await db["users"].insert_one(new_user_data.model_dump())
-            return str(result.inserted_id)
+            return new_user_data
     except Exception as e:
         print(f"Error creating user: {e}")
-        return ""
+        logger.exception("Error creating user")
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail="Error creating user")
+    
+async def get_user_by_supabase_id(mongo_client: MongoDBClient, supabase_id: str) -> User:
+    try:
+        async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
+            user_data = await db["users"].find_one(
+                {"supabase_id": supabase_id}, {"_id": 0}
+            )  # Exclude _id field, 1 would include it
+            return User.model_validate(user_data) if user_data else None
+    except Exception as e:
+        print(f"Error retrieving user information: {e}")
+        raise HTTPException(status_code=500, detail="Error retrieving user information")
 
 
 async def create_business_profile(
@@ -121,17 +129,17 @@ async def get_user_profile(
 
 
 async def get_user_business_profile_company_names(
-    mongo_client: MongoDBClient, email: str
+    mongo_client: MongoDBClient, supabase_id: str
 ) -> List[str] | None:
     try:
         async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
             business_profiles = await db["business_profiles"].find(
-                {"email": email}, {"_id": 0}
+                {"supabase_id": supabase_id}, {"_id": 0}
             )  # Exclude _id field, 1 would include it
             return [bp["company_name"] for bp in business_profiles]
     except Exception as e:
         print(f"Error retrieving user information: {e}")
-        return None
+        raise HTTPException(status_code=500, detail="Error retrieving user information")
 
 
 async def get_user_business_profiles(
@@ -149,12 +157,12 @@ async def get_user_business_profiles(
 
 
 async def get_business_profile_with_company_name(
-    mongo_client: MongoDBClient, email: str, company_name: str
+    mongo_client: MongoDBClient, supabase_id: str, company_name: str
 ) -> BusinessProfile | None:
     try:
         async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
             business_profiles = await db["business_profiles"].find_one(
-                {"email": email, "company_name": company_name}, {"_id": 0}
+                {"supabase_id": supabase_id, "company_name": company_name}, {"_id": 0}
             )  # Exclude _id field, 1 would include it
             return BusinessProfile.model_validate(business_profiles)
     except Exception as e:
@@ -163,28 +171,28 @@ async def get_business_profile_with_company_name(
 
 
 async def update_user_information(
-    mongo_client: MongoDBClient, email: str, user_update: UserUpdate
+    mongo_client: MongoDBClient, supabase_id: str, user_update: UserUpdate
 ) -> bool:
     """Update base user information in the database"""
 
     try:
         async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
             result = await db["users"].update_one(
-                {"email": email}, {"$set": user_update.model_dump(exclude_unset=True)}
+                {"supabase_id": supabase_id}, {"$set": user_update.model_dump(exclude_unset=True)}
             )
             return result.modified_count > 0
     except Exception as e:
         print(f"Error updating user information: {e}")
-        return False
+        raise HTTPException(status_code=500, detail="Error updating user information")
 
 
 async def add_business_profile_operation(
-    mongo_client: MongoDBClient, email: str, business_profile: BusinessProfile
+    mongo_client: MongoDBClient, supabase_id: str, business_profile: BusinessProfile
 ):
     try:
         async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
             result = await db["business_profiles"].update_one(
-                {"email": email},
+                {"supabase_id": supabase_id},
                 {
                     "$push": {"business_profiles": business_profile.model_dump()},
                 },
@@ -193,19 +201,19 @@ async def add_business_profile_operation(
             return result.modified_count > 0 or result.upserted_id is not None
     except Exception as e:
         print(f"Error updating business profile: {e}")
-        return False
+        raise HTTPException(status_code=500, detail="Error updating business profile")
 
 
 async def update_business_profile_operation(
     mongo_client: MongoDBClient,
-    email: str,
+    supabase_id: str,
     company_name: str,
     business_profile: UpdateBusinessProfile,
 ):
     try:
         async with mongo_client.get_db(secrets.mongo_db_database_name) as db:
             result = await db["users"].update_one(
-                {"email": email, "business_profile.company_name": company_name},
+                {"supabase_id": supabase_id, "business_profile.company_name": company_name},
                 {
                     "$set": {
                         "business_profile.$": business_profile.model_dump(
